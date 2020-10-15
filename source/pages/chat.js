ceea7251f7a88c12d31f0a7826f273ff93186295
ahmdirfansyah <ahmad.irfansyah.amd@gmail.com>
2020-10-15 11:14:49 +0700

    commit lagi

--- source/pages/chat.js
+++ source/pages/chat.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { View, Text, StyleSheet, Dimensions, ToastAndroid } from 'react-native';
-import { List, Colors, Avatar, Badge } from 'react-native-paper';
+import { List, Colors, Avatar, Badge, Divider } from 'react-native-paper';
 
 import user from '../assets/user.jpg'
 
@@ -8,9 +8,10 @@ const Chat = () => {
   return (
     <View>
         <List.Section>
-            {/* <List.Subheader>Some title</List.Subheader> */}
             <List.Item title="Ahmad Irfansyah" description="Assalamu'alaikum" left={props => <Avatar.Image size={55} source={user}  />} right={props => <Badge>1250</Badge> } onPress={()=>{ToastAndroid.show("Chat Pressed", ToastAndroid.SHORT)}}/>
+            <Divider />
             <List.Item title="M. Rizki Novriandri" description="Assalamu'alaikum" left={props => <Avatar.Text size={55} label="RN"  />} right={props => <Text>01.00</Text>} onPress={()=>{ToastAndroid.show("Chat Pressed", ToastAndroid.SHORT)}}/>
+            <Divider />
             <List.Item title="Mawan Irwansyah" description="Assalamu'alaikum" left={props => <Avatar.Icon size={55} icon="github"  />} right={props => <Text>02.00</Text>} onPress={()=>{ToastAndroid.show("Chat Pressed", ToastAndroid.SHORT)}}/>
         </List.Section>
     </View>
--- source/pages/status.js
+++ source/pages/status.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { View, Text, StyleSheet, Dimensions, ToastAndroid } from 'react-native';
-import { List, Colors, Avatar, Badge } from 'react-native-paper';
+import { List, Colors, Avatar, Badge, Divider } from 'react-native-paper';
 
 // import ApiContainer from './API/container';
 import user from '../assets/user.jpg'
@@ -8,17 +8,12 @@ import user from '../assets/user.jpg'
 const Status = () => {
 
   return (
-    // <>
-    //     <List.Section>
-    //         <List.Subheader>Status Page</List.Subheader>
-    //     </List.Section>
-    //     {/* <ApiContainer /> */}
-    // </>
     <View>
       <List.Section>
-          
           <List.Item title="My status" description="Tap to add status update" left={props => <Avatar.Image size={55} source={user}  />} onPress={()=>{ToastAndroid.show("Trying to Update Status", ToastAndroid.SHORT)}}/>
+          <Divider />
           <List.Subheader>Recent updates</List.Subheader>
+          <Divider />
           <List.Item title="Ahmad Irfansyah" description="7 Minutes ago" left={props => <Avatar.Image size={55} source={user}  />} onPress={()=>{ToastAndroid.show("Update shown", ToastAndroid.SHORT)}}/>
           <List.Item title="M. Rizki Novriandri" description="18 Minutes ago" left={props => <Avatar.Text size={55} label="RN"  />} onPress={()=>{ToastAndroid.show("Update shown", ToastAndroid.SHORT)}}/>
           <List.Item title="Mawan Irwansyah" description="Today, 8:27 AM" left={props => <Avatar.Icon size={55} icon="github"  />} onPress={()=>{ToastAndroid.show("Update shown", ToastAndroid.SHORT)}}/>
